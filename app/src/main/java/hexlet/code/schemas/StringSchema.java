51fcd4d35d2ed81df6ff389bb680f9b2b378b802
benzovvozh <augusthoffman3@gmail.com>
2024-12-26 18:44:50 +0500

    made methods not final in StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -3,17 +3,17 @@ package hexlet.code.schemas;
 public final class StringSchema extends BaseSchema<String> {
 
 
-    public final StringSchema required() {
+    public StringSchema required() {
         addCheck("required", value -> value != null && !(value.isEmpty()));
         return this;
     }
 
-    public final StringSchema minLength(Integer minLength) {
+    public StringSchema minLength(Integer minLength) {
         addCheck("minLength", value -> value != null && value.length() >= minLength);
         return this;
     }
 
-    public final StringSchema contains(String subString) {
+    public StringSchema contains(String subString) {
         boolean substringCheck;
         if (subString != null && !(subString.isEmpty())) {
             substringCheck = true;
